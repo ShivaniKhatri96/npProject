d384b8723153d1e6ca15ebf83a0aedf0ea2b765e
Shivani Khatri <khatri.shivani96@gmail.com>
2022-02-01 18:45:25 +0100

    header for desktop corrected

--- tourism/src/Components/miniComponents/Header.js
+++ tourism/src/Components/miniComponents/Header.js
@@ -25,6 +25,7 @@ import Grid from "@mui/material/Grid";
 import { useNavigate } from "react-router";
 import { useContext } from "react";
 import AuthKey from "../store/authKey";
+import Button from "@mui/material/Button";
 const Header = () => {
   let navigate = useNavigate();
   const authCtx = useContext(AuthKey);
@@ -62,13 +63,14 @@ const Header = () => {
       width: "auto",
     },
     [theme.breakpoints.up("xl")]: {
-      margin: theme.spacing(1, 2, 1, 0),
-      marginLeft: theme.spacing(3),
+      margin: theme.spacing(1.5, 0, 1.5, 0),
+      marginLeft: theme.spacing(0),
+      // marginLeft: theme.spacing(3),
       width: "auto",
     },
   }));
   const SearchIconWrapper = styled("div")(({ theme }) => ({
-    padding: theme.spacing(0, 2),
+    padding: theme.spacing(0, 4),
     height: "100%",
     position: "absolute",
     pointerEvents: "none",
@@ -82,14 +84,14 @@ const Header = () => {
     "& .MuiInputBase-input": {
       padding: theme.spacing(1, 1, 1, 0),
       // vertical padding and font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      paddingLeft: `calc(1em + ${theme.spacing(6)})`,
       transition: theme.transitions.create("width"),
       width: "100%",
       [theme.breakpoints.up("lg")]: {
         width: "15ch",
       },
       [theme.breakpoints.up("xl")]: {
-        width: "25ch",
+        width: "35ch",
       },
       marginTop: theme.spacing(0.5),
     },
@@ -99,7 +101,8 @@ const Header = () => {
     <AppBar elevation={1} sx={{ backgroundColor: "#03588C" }}>
       <Toolbar>
         <Grid container spacing={1}>
-          <Grid item xs={10} sm={8} md={9} lg={2} xl={1.25}>
+          <Grid item xl={0.5} />
+          <Grid item xs={10} sm={8} md={9} lg={2} xl={2.5}>
             {/* logo and search*/}
             <img
               src={require("../Pictures/logo.png")}
@@ -115,14 +118,17 @@ const Header = () => {
               <SearchIcon />
             </IconButton>
           </Grid>
-
+          <Grid item lg={0.5} xl={1.5} />
           <Grid
             item
             sm={4}
             md={3}
             lg={2}
-            xl={2.15}
-            sx={{ display: { xs: "none", sm: "flex" } }}
+            xl={3}
+            sx={{
+              display: { xs: "none", sm: "flex" },
+              justifyContent: "center",
+            }}
           >
             <Search>
               <SearchIconWrapper>
@@ -134,60 +140,159 @@ const Header = () => {
               ></StyledInputBase>
             </Search>
           </Grid>
-
-          <Grid item lg={0.5} xl={1.95}></Grid>
-
+          {!isLoggedIn && (
+            <Grid
+            item
+            xs={12}
+            lg={4}
+            xl={3.5}
+           sx={{ display: "flex", justifyContent: "end" }}
+          >
+            {/* before logging in */}
+            {!isLoggedIn && (
+              <Link
+                to="/connect"
+                style={{
+                  textDecoration: "none",
+                  cursor: "pointer",
+                  color: "white",
+                  display: "flex",
+                  alignSelf: "center",
+                }}
+              >
+                <MenuItem>
+                  <LoginIcon />
+                  &nbsp; Connect
+                </MenuItem>
+              </Link>
+            )}
+            </Grid>
+          )}
+          <Grid item lg={0.5} xl={0.5} />
           <Grid
             item
             xs={12}
             lg={4}
-            xl={1.5}
-            sx={{ display: "flex", justifyContent: "space-around" }}
+            xl={3.5}
+           sx={{ display: "flex", justifyContent: "space-around" }}
           >
-            {/* home page */}
-            <Link to="/">
-              <IconButton
-                sx={{
-                  marginRight: { lg: "10px", xl: "80px" },
-                  cursor: "pointer",
-                  color: "white",
-                  marginTop: { lg: "10px" },
-                }}
-              >
-                <HomeIcon />
-              </IconButton>
-            </Link>
-
-            {/* Community page/dashboard */}
-            {isLoggedIn && (
-              <Link to="/dashboard">
-                <IconButton
-                  sx={{
-                    marginRight: { lg: "10px", xl: "80px" },
-                    cursor: "pointer",
-                    color: "white",
-                    marginTop: { lg: "10px" },
-                  }}
-                >
-                  <GroupsIcon />
-                </IconButton>
-              </Link>
-            )}
             {/* profile page */}
             {isLoggedIn && (
-              <Link to="/profile">
-                <IconButton
-                  sx={{
-                    marginRight: { lg: "20px", xl: "80px" },
-                    cursor: "pointer",
-                    color: "white",
-                    marginTop: { lg: "10px" },
-                  }}
-                >
-                  <AccountCircleIcon />
-                </IconButton>
+              <Link
+                to="/profile"
+                style={{
+                  textDecoration: "none",
+                  cursor: "pointer",
+                  color: "white",
+                  display: "flex",
+                  alignSelf: "center",
+                }}
+              >
+                Profile
               </Link>
             )}
+            {/* Community page/dashboard */}
+            {isLoggedIn && (
+              <Link
+                to="/dashboard"
+                style={{
+                  textDecoration: "none",
+                  cursor: "pointer",
+                  color: "white",
+                  display: "flex",
+                  alignSelf: "center",
+                }}
+              >
+                Community
+              </Link>
+            )}
+            {isLoggedIn && (
+              <Link
+                to="/travelinfo"
+                style={{
+                  textDecoration: "none",
+                  cursor: "pointer",
+                  color: "white",
+                  display: "flex",
+                  alignSelf: "center",
+                }}
+              >
+                Travel Details
+              </Link>
+            )}
+            {/* <Grid
+            item
+            lg={2}
+            xl={2}
+            sx={{ display: { xs: "none", lg: "flex" }, justifyContent: "end" }}
+          > */}
+            {/* button Links */}
+            {/* notifications */}
+            {isLoggedIn && (
+              <Badge
+                sx={{
+                  marginTop: { lg: "20px", xl: "18px" },
+                  // marginLeft: { lg:"35px" ,xl:"35px" },
+                  // marginRight: { lg: "28px" },
+                  cursor: "pointer",
+                }}
+                badgeContent={1}
+                color="secondary"
+              >
+                <NotificationsIcon />
+              </Badge>
+            )}
+            {/* dropdown/hamburger menu */}
+            {isLoggedIn && (
+              <Badge
+                sx={{
+                  marginTop: { lg: "20px", xl: "18px" },
+                  cursor: "pointer",
+                }}
+                id="drop-down-badge"
+                aria-controls={open ? "drop-down-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+              >
+                <ArrowDropDownCircleIcon
+                  sx={{ display: { xs: "none", lg: "flex" } }}
+                />
+              </Badge>
+            )}
+            {/* dropdown list large screens*/}
+            {/* after logging in */}
+            {isLoggedIn && (
+              <Menu
+                id="drop-down-badge"
+                aria-labelledby="drop-down-badge"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+                transformOrigin={{ vertical: "top", horizontal: "right" }}
+              >
+                <Link
+                  to="/settings"
+                  style={{ textDecoration: "none", color: "#000000" }}
+                >
+                  <MenuItem onClick={handleClose}>
+                    <SettingsIcon />
+                    &nbsp; Settings
+                  </MenuItem>
+                </Link>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    logoutHandler();
+                  }}
+                >
+                  <LogoutIcon />
+                  &nbsp; Log out
+                </MenuItem>
+              </Menu>
+            )}
+            {/* </Grid> */}
             {/* only for screen less than lg */}
             {/* notifications */}
             {isLoggedIn && (
@@ -220,107 +325,10 @@ const Header = () => {
               <MenuSharpIcon sx={{ display: { xs: "flex", lg: "none" } }} />
             </Badge>
           </Grid>
-
+          <Grid item xl={0.5} />
           {/* for large screens icons */}
 
-          <Grid item lg={1} xl={3}></Grid>
-
-          <Grid
-            item
-            lg={2}
-            xl={2}
-            sx={{ display: { xs: "none", lg: "flex" }, justifyContent: "end" }}
-          >
-            {/* button Links */}
-            {/* notifications */}
-            {isLoggedIn && (
-              <Badge
-                sx={{
-                  marginTop: { lg: "20px", xl: "18px" },
-                  // marginLeft: { lg:"35px" ,xl:"35px" },
-                  marginRight: { lg: "28px" },
-                  cursor: "pointer",
-                }}
-                badgeContent={1}
-                color="secondary"
-              >
-                <NotificationsIcon />
-              </Badge>
-            )}
-            {/* dropdown/hamburger menu */}
-            <Badge
-              sx={{
-                marginTop: { lg: "20px", xl: "18px" },
-                cursor: "pointer",
-              }}
-              id="drop-down-badge"
-              aria-controls={open ? "drop-down-menu" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-            >
-              <ArrowDropDownCircleIcon
-                sx={{ display: { xs: "none", lg: "flex" } }}
-              />
-            </Badge>
-            {/* dropdown list */}
-            <Menu
-              id="drop-down-badge"
-              aria-labelledby="drop-down-badge"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              // anchorOrigin={{ vertical: "top", horizontal: "left" }}
-              // transformOrigin={{ vertical: "top", horizontal: "left" }}
-            >
-              {/* before logging in */}
-              {!isLoggedIn && (
-                <Link
-                  to="/connect"
-                  style={{ textDecoration: "none", color: "#000000" }}
-                >
-                  <MenuItem onClick={handleClose}>
-                    <LoginIcon />
-                    &nbsp; Connect
-                  </MenuItem>
-                </Link>
-              )}
-              {/* After logging in  */}
-              {isLoggedIn && (
-                <Link
-                  to="/travelinfo"
-                  style={{ textDecoration: "none", color: "#000000" }}
-                >
-                  <MenuItem onClick={handleClose}>
-                    <TravelExploreIcon />
-                    &nbsp; Travel info
-                  </MenuItem>
-                </Link>
-              )}
-              {isLoggedIn && (
-                <Link
-                  to="/settings"
-                  style={{ textDecoration: "none", color: "#000000" }}
-                >
-                  <MenuItem onClick={handleClose}>
-                    <SettingsIcon />
-                    &nbsp; Settings
-                  </MenuItem>
-                </Link>
-              )}
-              {isLoggedIn && (
-                <MenuItem
-                  onClick={() => {
-                    handleClose();
-                    logoutHandler();
-                  }}
-                >
-                  <LogoutIcon />
-                  &nbsp; Log out
-                </MenuItem>
-              )}
-            </Menu>
-          </Grid>
+          {/* <Grid item lg={1} xl={3}></Grid> */}
         </Grid>
       </Toolbar>
     </AppBar>
