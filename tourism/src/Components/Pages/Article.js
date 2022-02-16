90ae19f154ee88e75b6611a038e6a7d01e4b987b
Shivani Khatri <khatri.shivani96@gmail.com>
2022-02-16 13:20:26 +0100

    article page code cleaned

--- tourism/src/Components/Pages/Article.js
+++ tourism/src/Components/Pages/Article.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Grid";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import { Typography } from "@mui/material";
 import { Art, Grid1, Grid2, Typo } from "../../styles/ArticleStyle";
 const Article = () => {
   const [one, setOne] = useState([]);
@@ -25,7 +22,6 @@ const Article = () => {
   console.log(one);
   return (
     <div>
-      {/* marginTop:"140px" */}
       <Grid1 container spacing={2}>
         <Grid item xs={12} md={8} lg={7} xl={6}>
           <Card>
@@ -44,41 +40,6 @@ const Article = () => {
           </Card>
         </Grid>
       </Grid1>
-      {/* <Grid
-        container
-        spacing={2}
-        sx={{ display: "flex", justifyContent: "center", marginTop: "80px" }}
-      >
-        <Grid item xs={12} lg={7} xl={6}>
-          <Card>
-            <Grid
-              container
-              spacing={2}
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "30px",
-              }}
-            >
-              <Grid item xs={11}>
-              
-                <CardMedia
-                  component="img"
-                  height="400"
-                  image={process.env.PUBLIC_URL + one.img}
-                />
-               <Typography variant="h1" component="div" className="heading"> {one.name}</Typography>
-                        <CardContent>
-                       {one.content} {one.learnMore} 
-                        </CardContent>
-                        <CardContent>
-                    {one.learnMore}
-                        </CardContent>
-              </Grid>
-            </Grid>
-          </Card>
-        </Grid>
-      </Grid> */}
     </div>
   );
 };
